f27c0c0af585fce2cbd5f30a8e8e289d61472c95
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-08-03 15:55:04 +0300

    set up sqlalchemy core abstractions

---
+++ bot/db/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase
+
+
+class ModelBase(AsyncAttrs, DeclarativeBase):
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(table_name)s_%(column_0_N_name)s",
+            "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_N_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
