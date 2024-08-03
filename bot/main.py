e080be0c216c2e7e5379ff2d0e13c2aba38ed774
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-08-03 17:00:39 +0300

    show typing char action on openai request

--- bot/main.py
+++ bot/main.py
@@ -3,11 +3,12 @@ import logging
 
 from aiogram import Bot, Dispatcher, F, Router
 from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
+from aiogram.enums import ChatAction, ParseMode
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.utils import markdown
+from aiogram.utils.chat_action import ChatActionSender
 
 from bot.config import settings
 from bot.db.queries import add_user
@@ -33,15 +34,17 @@ async def chat_button_handler(message: Message, state: FSMContext) -> None:
 
 @root_router.message(ChatSurvey.query)
 async def chat_query_state_handler(message: Message, state: FSMContext) -> None:
-    await state.set_state(ChatSurvey.wait)
-    result = await generate_text(query=message.text)
-    await message.answer(text=result)
-    await state.clear()
+    async with ChatActionSender.typing(bot=message.bot, chat_id=message.chat.id):
+        await state.set_state(ChatSurvey.wait)
+        result = await generate_text(query=message.text)
+        await message.answer(text=result)
+        await state.clear()
 
 
 @root_router.message(ChatSurvey.wait)
 async def chat_wait_state_handler(message: Message) -> None:
     await message.answer(text="⏳ Processing previous query. Please wait...")
+    await message.bot.send_chat_action(chat_id=message.chat.id, action=ChatAction.TYPING)
 
 
 async def main() -> None:
