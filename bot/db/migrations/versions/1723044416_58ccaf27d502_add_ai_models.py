72bf7b374c0b7abca67ae307c58b8b0855ac5911
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-08-07 19:15:44 +0300

    populate ai_models table

---
+++ bot/db/migrations/versions/1723044416_58ccaf27d502_add_ai_models.py
@@ -0,0 +1,46 @@
+"""Add ai_models.
+
+Revision ID: 58ccaf27d502
+Revises: 0fc83e0cdbdc
+Create Date: 2024-08-07 15:26:56.040117+00:00
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+revision: str = "58ccaf27d502"
+down_revision: Union[str, None] = "0fc83e0cdbdc"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    conn = op.get_bind()
+    type_id = conn.scalar(sa.text("SELECT id FROM ai_types WHERE name = 'text'"))
+    conn.execute(
+        sa.text("""
+            INSERT INTO ai_models (name, type_id, price) VALUES
+            ('gpt-3.5-turbo', :type_id, 0.000009),
+            ('gpt-4', :type_id, 0.00009),
+            ('gpt-4-turbo', :type_id, 0.00004),
+            ('gpt-4o', :type_id, 0.00002),
+            ('gpt-4o-mini', :type_id, 0.00000075)
+        """),
+        {"type_id": type_id},
+    )
+
+
+def downgrade() -> None:
+    op.execute("""
+        DELETE FROM ai_models 
+        WHERE name IN (
+            'gpt-3.5-turbo', 
+            'gpt-4', 
+            'gpt-4-turbo', 
+            'gpt-4o', 
+            'gpt-4o-mini'
+        )
+    """)
